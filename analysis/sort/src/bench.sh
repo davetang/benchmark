c3452d2e3f5540d886c68db8228d9a287a745ebf
Dave Tang <davetingpongtang@gmail.com>
2024-01-27 18:14:51 +0900

    Rearrange output

--- analysis/sort/src/bench.sh
+++ analysis/sort/src/bench.sh
@@ -15,8 +15,8 @@ for s in ${SIZES[@]}; do
    for i in {1..3}; do
       for CPU in 1 2 4 8 12 16; do
          for MEM in 4G 8G 12G; do
-            REALTIME=$(/usr/bin/time --format %e sort --parallel ${CPU} --buffer-size=${MEM} ${DIR}/data/${s}.txt 2>&1 > /dev/null)
-            echo -e "${i},${s},${CPU},${MEM},${REALTIME}"
+            REALTIME=$(/usr/bin/time --format %e sort --parallel ${CPU} --buffer-size ${MEM} ${DIR}/data/${s}.txt 2>&1 > /dev/null)
+            echo -e "${s},${i},${CPU},${MEM},${REALTIME}"
          done
       done
    done

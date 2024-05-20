96f2cf6dfde5cc0fea0286a78f2f125ec17e402c
Dave Tang <davetingpongtang@gmail.com>
2024-05-20 16:16:48 +0900

    Additional datasets

--- data/10x/download.sh
+++ data/10x/download.sh
@@ -17,5 +17,11 @@ download https://cf.10xgenomics.com/samples/cell-exp/1.1.0/jurkat/jurkat_filtere
 download https://cf.10xgenomics.com/samples/cell-exp/1.1.0/293t/293t_filtered_gene_bc_matrices.tar.gz
 download https://cf.10xgenomics.com/samples/cell-exp/1.1.0/jurkat:293t_50:50/jurkat:293t_50:50_filtered_gene_bc_matrices.tar.gz
 
+# Peripheral blood mononuclear cells (PBMCs) from a healthy donor (same donor as pbmc8k)
+download https://cf.10xgenomics.com/samples/cell-exp/2.1.0/pbmc4k/pbmc4k_filtered_gene_bc_matrices.tar.gz
+
+# 3k Pan T Cells from a Healthy Donor
+download https://cf.10xgenomics.com/samples/cell-exp/2.1.0/t_3k/t_3k_filtered_gene_bc_matrices.tar.gz
+
 >&2 echo Done
 exit 0
